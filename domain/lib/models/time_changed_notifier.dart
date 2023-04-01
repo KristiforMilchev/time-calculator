98821f4b91c22c5f0889c27f457645b6ba48d37a
kristifor milchev <km@airzen.io>
2023-04-01 19:32:01 +0300

    adding UTC offset control with the slider for each TimeZone.

---
+++ domain/lib/models/time_changed_notifier.dart
@@ -0,0 +1,11 @@
+class TimeChangedNotifier {
+  int hours;
+  int minutes;
+  double offset;
+
+  TimeChangedNotifier({
+    required this.hours,
+    required this.minutes,
+    required this.offset,
+  });
+}
---
+++ domain/lib/models/time_zone.dart
@@ -0,0 +1,6 @@
+class TimeZone {
+  final String name;
+  final double offset;
+
+  const TimeZone({required this.name, required this.offset});
+}
